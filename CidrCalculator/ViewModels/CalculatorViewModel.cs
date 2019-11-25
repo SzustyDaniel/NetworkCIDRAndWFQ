6fb81c3d40814e723cae3a80eaeef8c9c4a5328c
Daniel Szuster <37206928+SzustyDaniel@users.noreply.github.com>
2019-11-25 14:16:50 +0200

    Removed unecessary dll

--- CidrCalculator/ViewModels/CalculatorViewModel.cs
+++ CidrCalculator/ViewModels/CalculatorViewModel.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Prism.Mvvm;
 using Prism.Commands;
-using HomeWorkHelper;
+using CidrCalculator.Services;
 
 namespace CidrCalculator.ViewModels
 {
